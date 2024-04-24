9a0617532b34f1cff61fec80bfe47e5d29c31ab6
patrick okafor <patrickpeters911@gmail.com>
2024-04-24 14:49:18 -0700

    fixed square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Module """
 
 class Square():
     
     def __init__(self, width=0, height=0):
+        """Initialize"""
         self.width = width
         self.height = height
 
